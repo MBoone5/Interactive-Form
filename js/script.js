bf205933ea6e77094de5d73024d43d56a795e718
MBoone5 <moses.s.boone@gmail.com>
2018-11-19 12:09:55 -0800

    Job Select form functional

--- js/script.js
+++ js/script.js
@@ -8,35 +8,54 @@ $(document).ready(() => {
 	$("input:text:visible:first").focus();
 
 	//ALL FORMS ARE UNOBTRUSIVE
-	// Creating a varialbe for 'other' option
-	let $otherOption = $("option:contains('Other')");
+	// Creating variables for important "select" form elements
+	const $jobSelectForm = $("#title");
+	const $otherOption = $("option[value='other']");
 
-	// conditional statement for if 'other' is selected
-	if ($($otherOption + ":selected")) {
+	// Creating 'other' response Element
+	const $otherTitle = $("<input>")
+	$($otherTitle).attr('id', 'other-title').attr('type', 'text').attr('placeholder', 'Your Job Title...')
 
-	} else {
+	// Adding 'other-title' to document
+	$jobSelectForm.after($otherTitle)
 
-	}
+	//Hiding the 'other-title' form for unobtrusive-ness
+	$otherTitle.hide();
 
-		//'Design' and 'Color' fields are hidden until the previous field has recieved input
-		//T-Shirt color menu that has conditional options based on the chosen design
-			//JS Puns - Conrflower Blue, Dark Slate Grey, Gold
-			//Heart JS - Tomato, Steel Blue, Dim Grey
-	 	//Color menu updates if design changes
+	// event listener for job select form
+	$jobSelectForm.change( (event) => {
+		let $currentOption = $(event.target).val();
+
+		if ($currentOption === "other") {
+			$otherTitle.show();
+		}else {
+			$otherTitle.hide();
+		}
+	});
+
+	//T-Shirt color menu that has conditional options based on the chosen design
+	//JS Puns - Conrflower Blue, Dark Slate Grey, Gold
+	//Heart JS - Tomato, Steel Blue, Dim Grey
+	//Color menu updates if design changes
+
+	//Important variables for T-Shirt select form
+	const $designSelectForm = $("#design");
+	const $colorSelectForm = $("#color");
 
 
-		//Activities section: Events cannot conflict, and the sum of admission on the fly
-			//Workshop time cannot conflict, and the conflictions are updated with changes in input
-				//Tues 0900-1200: JS Frameworks, Express
-				//Tues 1300-1600: JS Libraries, Node.js
 
-		//Payment section that shows chosen payment method div based on selection
-			//I.e. if Bitcoin is chosen, the fields for paypal and card are hidden
+	//Activities section: Events cannot conflict, and the sum of admission on the fly
+	//Workshop time cannot conflict, and the conflictions are updated with changes in input
+		//Tues 0900-1200: JS Frameworks, Express
+		//Tues 1300-1600: JS Libraries, Node.js
 
-	 	//Validated all of the above forms on the fly, and before letting the user post the form
-			//All form validatoin error messages must be custom and not HTML5 based
-			//Error messages are conditional
-				//i.e. If the field has some content, but incorrect content, the error message specifies the issue
+	//Payment section that shows chosen payment method div based on selection
+	//I.e. if Bitcoin is chosen, the fields for paypal and card are hidden
+
+	//Validated all of the above forms on the fly, and before letting the user post the form
+	//All form validatoin error messages must be custom and not HTML5 based
+	//Error messages are conditional
+		//i.e. If the field has some content, but incorrect content, the error message specifies the issue
 
 
 
